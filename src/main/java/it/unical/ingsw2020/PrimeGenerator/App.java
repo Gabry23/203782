773b2052b2b90d2ec5852992a8c2a8f90e178b73
Gabriele <gabry-forga_99@live.it>
2020-10-28 11:42:30 +0100

    Initial commit

---
+++ src/main/java/it/unical/ingsw2020/PrimeGenerator/App.java
@@ -0,0 +1,14 @@
+package it.unical.ingsw2020.PrimeGenerator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        MyMath math = new MyMath();
+        System.out.println(math.primeNumbersGenerator(100));
+    }
+}
---
+++ src/main/java/it/unical/ingsw2020/PrimeGenerator/MyMath.java
@@ -0,0 +1,33 @@
+package it.unical.ingsw2020.PrimeGenerator;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MyMath {
+
+	public List<Integer> primeNumbersGenerator(int n) {
+		List<Integer> primeNumbers = new LinkedList<>();
+		if(n>=2) {
+			primeNumbers.add(2);
+		}
+		
+		for(int i=3; i<=n; i+=2) {
+			if(isPrime(i))
+				primeNumbers.add(i);
+		}
+		return primeNumbers;
+	}
+
+	private boolean isPrime(int number) {
+		if(number<2) {
+			return false;
+		}
+		
+		for(int i=2; i*2<number;i++) {
+			if(number%i==0)
+				return false;
+		}
+		return true;
+	}
+
+}
